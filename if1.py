ce2257a4607bf7e00d464d4e38715d6fe8a949b3
jssemeone <jssemeone@mail.ru>
2019-05-21 17:05:32 +0300

    Добавил файл для примера работы с git

---
+++ if1.py
@@ -0,0 +1,12 @@
+age = int(input('Введите ваш возраст - '))
+
+if age > 100:
+    print('Не верю')
+elif age >= 24:
+    print('Жизнь - боль')
+elif age >= 18:
+    print('Все впереди(нет)')
+elif age >= 7:
+    print('Добро пожаловать')
+elif age < 7:
+    print('Дет. сад, дружище')
