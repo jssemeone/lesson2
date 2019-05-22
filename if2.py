36a87a21b579a2adb0681d190a7eec100542b74f
jssemeone <jssemeone@mail.ru>
2019-05-22 18:21:39 +0300

    Код вроде работает, но что то не так

--- if2.py
+++ if2.py
@@ -3,23 +3,26 @@ def compare(line1, line2):
     if len(line1) == len(line2):
         return 1
 
-    # elif len(line1) > len(line2):
-    #     return 2
+    elif len(line1) > len(line2):
+        return 2
 
-    # elif len(line1) != len(line2):
+    elif len(line1) != len(line2):
 
-    #     if line2 == "learn":
-    #         return 3
+        if line2 == "learn":
+            return 3
 
-    # variant = isinstance(line1, line2, str)
+    line1 = isinstance(line1, str)
+    line2 = isinstance(line2, str)
 
-    # if variant == True:
-    #     return 0
+    if line1 == True:
+        if line2 == True:
+            return 0
+    
 
 line1 = input("Введите первое слово - ")
 
 line2 = input("Введите второе слово - ")
 
-result = compare
+result = compare(line1, line2)
 
 print(result)
